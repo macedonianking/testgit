078f80ce0aadf3fba2d909fccdf4c15a39dc748f
mikie <lhb198710@gmail.com>
2014-05-11 14:02:50 +0800

    branch third message

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
 {
 	std::cout << "This is a new message" << std::endl;
 	std::cout << "This is a second message and a tab" << std::endl;
+	std::cout << "branch third message" << std::endl;
 
 	return 0;
 }
